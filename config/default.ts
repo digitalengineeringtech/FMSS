dc4220024c02a53e827f0a7893c396f7c730e079
HtooKhant-vexix <vexixongaming02>
2024-06-17 11:13:12 +0700

    G

--- config/default.ts
+++ config/default.ts
@@ -1,8 +1,10 @@
 export default {
   port: 9000,
   host: "localhost",
+  // dbUrl:
+  //   "mongodb://detpos:asdffdsa@192.168.1.146:27017/local-pos?authSource=admin",
   dbUrl:
-    "mongodb://detpos:asdffdsa@192.168.1.146:27017/local-pos?authSource=admin",
+    "mongodb://detpos:asdffdsa@192.168.0.100:27017/local-pos?authSource=admin",
   saltWorkFactor: 10,
   secretKey: "suuhh",
   page_limit: 50,
@@ -13,7 +15,7 @@ export default {
   mqttPassword: "asdffdsa",
   wsServerUrl: "http://13.251.206.31:9000/api/change-mode",
   tankDataUrl: "http://192.168.0.109:8080/baseOilcan",
-  // detailsaleCloudUrl: "https://detfsmm.com/api/detail-sale",
+  detailsaleCloudUrl: "https://detfsmm.com/api/detail-sale",
   // detailsaleCloudUrl: "http://192.168.1.146:9000/api/detail-sale",
   //coustomerCloudUrl: "http://detfsmm.com:9000/api/customer",
   // coustomerCloudUrl: "https://detfsmm.com/api/customer/local-create",
