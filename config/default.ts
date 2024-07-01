a956c84c6b48bb4cddb097993f3fccfeadb3a7e9
HtooKhant-vexix <vexixongaming02>
2024-07-01 16:23:41 +0700

    G

--- config/default.ts
+++ config/default.ts
@@ -1,16 +1,16 @@
 export default {
   port: 9000,
   host: "localhost",
-  // dbUrl:
-  //   "mongodb://detpos:asdffdsa@192.168.1.146:27017/local-pos?authSource=admin",
   dbUrl:
-    "mongodb://detpos:asdffdsa@192.168.0.100:27017/local-pos?authSource=admin",
+    "mongodb://detpos:asdffdsa@192.168.1.146:27017/local-pos?authSource=admin",
+  // dbUrl:
+  //   "mongodb://detpos:asdffdsa@192.168.0.100:27017/local-pos?authSource=admin",
   saltWorkFactor: 10,
   secretKey: "suuhh",
   page_limit: 50,
   // mqttUrl: "ws://127.0.0.1:9001",
-  mqttUrl: "mqtt://127.0.0.1:1883",
-  // mqttUrl: "mqtt://192.168.1.146:1883",
+  // mqttUrl: "mqtt://127.0.0.1:1883",
+  mqttUrl: "mqtt://192.168.1.146:1883",
   mqttUserName: "detpos",
   mqttPassword: "asdffdsa",
   wsServerUrl: "http://13.251.206.31:9000/api/change-mode",
