2bc220b4ff3b4e05c83c1e495b1362a321d1e3bb
HooKhant-vexix <vexixongaming02@gmail.com>
2024-11-11 11:50:44 +0700

    g

--- config/default.ts
+++ config/default.ts
@@ -19,24 +19,24 @@ export default {
 
   //--- tank data ( local atg ) -------------------------------------------------------
   //if atg => " tankDataUrl actual link " : " "
-  // tankDataUrl: "",
+  tankDataUrl: "",
   // tankDataUrl: "https://e688ad90-86cd-43c9-a524-2aaccb212b97.mock.pstmn.io/data",
-  tankDataUrl: "http://192.168.0.105:8080/baseOilcan",
+  // tankDataUrl: "http://192.168.0.105:8080/baseOilcan",
 
   //--- tank data ( tank data cloud ) -------------------------------------------------------
   tankDataCloudUrl: "https://detfsmm.com/api/tank-data",
-  // tankDataCloudUrl: "http://127.0.01:8000/api/tank-data",
+  // tankDataCloudUrl: "http://127.0.0.1:8000/api/tank-data",
 
   //--- tank data ( fuel in data cloud ) -------------------------------------------------------
   fuelInCloud: "https://detfsmm.com/api/fuelIn",
-  // fuelInCloud: "http://127.0.01:8000/api/fuelIn",
+  // fuelInCloud: "http://127.0.0.1:8000/api/fuelIn",
 
-  atgFuelInCloud: 'https://detfsmm.com/api/fuelIn/cloud/atg',
-    // atgFuelInCloud: 'http://127.0.01:8000/api/fuelIn/cloud/atg',
+  atgFuelInCloud: "https://detfsmm.com/api/fuelIn/cloud/atg",
+  // atgFuelInCloud: 'http://127.0.0.1:8000/api/fuelIn/cloud/atg',
 
   //--- tank data ( voucher data cloud ) -------------------------------------------------------
   detailsaleCloudUrl: "https://detfsmm.com/api/detail-sale",
-  // detailsaleCloudUrl: "http://127.0.01:8000/api/detail-sale",
+  // detailsaleCloudUrl: "http://127.0.0.1:8000/api/detail-sale",
 
   //coustomerCloudUrl: "http://detfsmm.com:9000/api/customer",
   // coustomerCloudUrl: "https://detfsmm.com/api/customer/local-create",
