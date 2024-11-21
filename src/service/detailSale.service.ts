7c34f6fb21d09d55bab415a28af31bda39ced083
Htet Aung Khant <htetaungkhant@securelinkmm.com>
2024-11-21 12:10:18 +0630

    detail sale creditReturn customer credit id bug fix

--- src/service/detailSale.service.ts
+++ src/service/detailSale.service.ts
@@ -550,7 +550,7 @@ export const detailSaleUpdateByDevice = async (
             await customerCredit.save();
 
             await creditReturnModel.create({
-                cutomerCredit: customerCredit._id,
+                customerCredit: customerCredit._id,
                 vocono: result.vocono,
                 creditAmount: result.totalPrice,
                 creditDueDate: customerCredit.creditDueDate
